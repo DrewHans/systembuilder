dba4a353ff38ebfbe45d1e263071c7236819e5a3
Drew Hans <drewhans555@gmail.com>
2021-07-25 22:33:51 -0500

    Make code --install-extension run as user

--- installer.sh
+++ installer.sh
@@ -75,10 +75,19 @@ echo ""
 # check if vscode is installed
 command -v code >/dev/null 2>&1 && {
     echo "installing vscode extensions"
-    code --install-extension material-icon-theme
-    code --install-extension ms-vscode.cpptools
-    code --install-extension ms-python.python
-    code --install-extension ms-python.vscode-pylance
+
+    sudo -u ${sudo_user_username} \
+        code --install-extension material-icon-theme
+
+    sudo -u ${sudo_user_username} \
+        code --install-extension ms-vscode.cpptools
+
+    sudo -u ${sudo_user_username} \
+        code --install-extension ms-python.python
+
+    sudo -u ${sudo_user_username} \
+        code --install-extension ms-python.vscode-pylance
+
     echo ""
 }
 
