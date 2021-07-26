e9c4f4322953648d18e135e613bd86f6447013ff
DrewHans <programmer.drew.hans@gmail.com>
2021-07-25 20:40:18 -0500

    Update nordvpn preinstall

--- apps/nordvpn/preinstall.sh
+++ apps/nordvpn/preinstall.sh
@@ -3,3 +3,6 @@
 curl -O https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
 
 sudo apt-get install ./nordvpn-release_1.0.0_all.deb --yes
+
+sudo rm -f ./nordvpn-release_1.0.0_all.deb
+
