f8849710b54c4d042261ddeaa42cfa238e551c73
DrewHans <programmer.drew.hans@gmail.com>
2021-09-03 18:12:03 -0500

    Add mpv, pavucontrol, and pulseeffects scripts to apps

---
+++ apps/mpv/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# if flatpak is on system
+command -v flatpak >/dev/null 2>&1 && {
+    # install through flatpak
+    sudo -u ${SUDO_USER:-$USER} \
+        flatpak install flathub io.mpv.Mpv
+}
+
+# if flatpak is not found
+command -v flatpak >/dev/null 2>&1 || {
+    echo "MPV install.sh: flatpak not found; skipping install."
+}
