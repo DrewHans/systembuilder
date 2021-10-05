88b2d998e4dad2ae2b441447b1bc84010b04160d
DrewHans <programmer.drew.hans@gmail.com>
2021-10-05 13:32:50 -0500

    Add yubikey related apps

---
+++ apps/yubikey/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+sudo apt install \
+    yubikey-manager \
+    yubikey-personalization-gui \
+    libpam-yubico \
+    libpam-u2f \
+    --yes
+
+# https://support.yubico.com/hc/en-us/articles/360016649039-Enabling-the-Yubico-PPA-on-Ubuntu
+# https://support.yubico.com/hc/en-us/articles/360016649099-Ubuntu-Linux-Login-Guide-U2F
+# if using cryptdisk, remember to move ~/.config/Yubico/u2f_keys file to /etc/Yubico/u2f_keys
