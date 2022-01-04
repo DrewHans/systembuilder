6160149498bc3be5f9f25005f884e2051e8f1951
DrewHans <programmer.drew.hans@gmail.com>
2022-01-03 20:53:05 -0600

    Add calibre install script

---
+++ apps/calibre/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# if flatpak is on system
+command -v flatpak >/dev/null 2>&1 && {
+    # install through flatpak
+    sudo -u ${SUDO_USER:-$USER} flatpak install flathub com.calibre_ebook.calibre --assumeyes
+}
+
+# if flatpak is not found
+command -v flatpak >/dev/null 2>&1 || {
+    echo "Error: flatpak not found; you need to manually install Calibre;"
+}
