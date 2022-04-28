cd41d968ce597ea386f5b50991915da69d64902e
DrewHans <programmer.drew.hans@gmail.com>
2022-04-27 22:10:12 -0500

    Add setup_elixir_lang.sh

---
+++ utils/setup_elixir_lang.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+echo "Warning: this didn't work on Ubuntu 22.04 when I last tried it"
+echo "Press enter to continue:"
+read tmpvar
+
+
+wget https://packages.erlang-solutions.com/erlang-solutions_2.0_all.deb
+
+
+if [ ! -f "erlang-solutions_2.0_all.deb" ]; then
+    echo "Failed to download erlang-solutions_2.0_all.deb file; aborting"
+    exit 1
+fi
+
+sudo dpkg -i erlang-solutions_2.0_all.deb || {
+    echo "Failed to install erlang-solutions_2.0_all.deb; aborting"
+    exit 1
+}
+
+sudo apt update
+
+sudo apt install esl-erlang elixir
+
