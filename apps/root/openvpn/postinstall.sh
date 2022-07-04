ad1a1a701be81db478a005d4bb941829ca776bcf
DrewHans <programmer.drew.hans@gmail.com>
2022-07-04 15:50:51 -0500

    Fix permissions for auth file

--- apps/root/openvpn/postinstall.sh
+++ apps/root/openvpn/postinstall.sh
@@ -11,17 +11,7 @@ rm /etc/openvpn/ovpn.zip
 # setup auth file to hold credentials
 touch /etc/openvpn/nordvpn_auth.txt
 chown root:root /etc/openvpn/nordvpn_auth.txt
-chmod 644 /etc/openvpn/nordvpn_auth.txt
+chmod 600 /etc/openvpn/nordvpn_auth.txt
 echo "REPLACE_WITH_VPN_SERVICE_USERNAME" >> /etc/openvpn/nordvpn_auth.txt
 echo "REPLACE_WITH_VPN_SERVICE_PASSWORD" >> /etc/openvpn/nordvpn_auth.txt
 # note: the service username & password are different from the account login
-
-# now you can start openvpn by running a command like:
-# sudo openvpn \
-# --config /etc/openvpn/ovpn_udp/us9632.nordvpn.com.udp.ovpn \
-# --auth-user-pass /etc/openvpn/nordvpn_auth.txt \
-# --log /etc/openvpn/log.txt \
-# --daemon
-
-
-
