1b68bb0f09cbfc3d3642b9726cdab41846e3f12c
DrewHans <programmer.drew.hans@gmail.com>
2022-07-12 19:34:32 -0500

    Automate vscode extension installation (again)

---
+++ apps/user/vscode/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+
+# do nothing as user, vscode gets installed via apt (for now)
---
+++ apps/user/vscode/postinstall.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+
+# check vscode is installed (should have installed via apt)
+command -v code >/dev/null 2>&1 && {
+    echo "Installing Visual Studio Code extensions"
+
+    code --install-extension material-icon-theme
+    code --install-extension ms-azuretools.vscode-docker
+    code --install-extension ms-dotnettools.csharp
+    code --install-extension ms-python.python
+    code --install-extension ms-python.vscode-pylance
+    code --install-extension ms-vscode.cpptools
+    code --install-extension platformio.platformio-ide
+
+    echo "Visual Studio Code extensions have been installed"
+    echo ""
+}
+
+# output message if not installed
+command -v code >/dev/null 2>&1 || {
+    echo "Visual Studio Code is not installed"
+    echo "Skipping vscode extensions installation"
+    echo ""
+}
