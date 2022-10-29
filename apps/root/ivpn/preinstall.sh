52cc4d3a51a14a583b2b804109bdebb6b022628f
DrewHans <programmer.drew.hans@gmail.com>
2022-10-28 19:23:24 -0500

    Update setup_docker.sh

--- apps/root/ivpn/preinstall.sh
+++ apps/root/ivpn/preinstall.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 
+if [ ! -f "/usr/share/keyrings/ivpn-archive-keyring.gpg" ]; then
+	# Add IVPN's GPG key
+	curl -fsSL https://repo.ivpn.net/stable/ubuntu/generic.gpg | gpg --dearmor > ~/ivpn-archive-keyring.gpg
+	mv ~/ivpn-archive-keyring.gpg /usr/share/keyrings/ivpn-archive-keyring.gpg
+fi
+
+if [ ! -f "/etc/apt/sources.list.d/ivpn.list" ]; then
+	# Add the IVPN repository
+	curl -fsSL https://repo.ivpn.net/stable/ubuntu/generic.list | tee /etc/apt/sources.list.d/ivpn.list
+fi
+
 # Source: https://www.ivpn.net/apps-linux/#ubuntu
-
-# Add IVPN's GPG key
-curl -fsSL https://repo.ivpn.net/stable/ubuntu/generic.gpg | gpg --dearmor > ~/ivpn-archive-keyring.gpg
-mv ~/ivpn-archive-keyring.gpg /usr/share/keyrings/ivpn-archive-keyring.gpg
-
-
-# Add the IVPN repository
-curl -fsSL https://repo.ivpn.net/stable/ubuntu/generic.list | tee /etc/apt/sources.list.d/ivpn.list
--- extras/setup_docker.sh
+++ extras/setup_docker.sh
@@ -14,7 +14,7 @@ command -v docker >/dev/null 2>&1 && {
 # Set up the repository
 sudo apt install ca-certificates curl gnupg lsb-release --yes
 
-if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
+if [ ! -f "/etc/apt/keyrings/docker.gpg" ]; then
 	sudo mkdir -p /etc/apt/keyrings
 
 	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -22,7 +22,7 @@ if [ ! -f /etc/apt/keyrings/docker.gpg ]; then
 	sudo chmod a+r /etc/apt/keyrings/docker.gpg
 fi
 
-if [ ! -f /etc/apt/sources.list.d/docker.list ]; then
+if [ ! -f "/etc/apt/sources.list.d/docker.list" ]; then
 	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian bullseye stable" \
 	| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 fi
