19237cff6e80b8694d2ff09b403243c886941015
DrewHans <programmer.drew.hans@gmail.com>
2023-01-01 15:17:15 -0600

    Fix log not writing

--- pop-os/installer.sh
+++ pop-os/installer.sh
@@ -15,42 +15,42 @@ check_is_root
 echo "Starting $0"
 
 # install dependencies
-sudo bash ./scripts/install-deps.sh  | tee -a ~/systembuilder_output.log
+sudo bash ./scripts/install-deps.sh  | tee -a /home/nyancat/systembuilder_output.log
 
 # create links
-sudo bash ./scripts/create-links.sh  | tee -a ~/systembuilder_output.log
+sudo bash ./scripts/create-links.sh  | tee -a /home/nyancat/systembuilder_output.log
 
 # set dns addresses
-sudo bash ./scripts/set-dns-addresses.sh  | tee -a ~/systembuilder_output.log
+sudo bash ./scripts/set-dns-addresses.sh  | tee -a /home/nyancat/systembuilder_output.log
 
 # extend sudo timeout
 sudo -v
 
 # install root apps
-sudo bash ./scripts/install-root-apps.sh  | tee -a ~/systembuilder_output.log
+sudo bash ./scripts/install-root-apps.sh  | tee -a /home/nyancat/systembuilder_output.log
 
 # extend sudo timeout
 sudo -v
 
 # install user apps
-sudo -u ${SUDO_USER} bash ./scripts/install-user-apps.sh  | tee -a ~/systembuilder_output.log
+sudo -u ${SUDO_USER} bash ./scripts/install-user-apps.sh  | tee -a /home/nyancat/systembuilder_output.log
 
 # extend sudo timeout
 sudo -v
 
 # install user dotfiles
-sudo -u ${SUDO_USER} bash ./scripts/install-dotfiles.sh  | tee -a ~/systembuilder_output.log
+sudo -u ${SUDO_USER} bash ./scripts/install-dotfiles.sh  | tee -a /home/nyancat/systembuilder_output.log
 
 # install user shellscripts
-sudo -u ${SUDO_USER} bash ./scripts/install-shellscripts.sh  | tee -a ~/systembuilder_output.log
+sudo -u ${SUDO_USER} bash ./scripts/install-shellscripts.sh  | tee -a /home/nyancat/systembuilder_output.log
 
 # install user directories
-sudo -u ${SUDO_USER} bash ./scripts/create-directories.sh  | tee -a ~/systembuilder_output.log
+sudo -u ${SUDO_USER} bash ./scripts/create-directories.sh  | tee -a /home/nyancat/systembuilder_output.log
 
 # extend sudo timeout
 sudo -v
 
 # set user gsettings
-sudo -u ${SUDO_USER} bash ./scripts/set-gsettings.sh  | tee -a ~/systembuilder_output.log
+sudo -u ${SUDO_USER} bash ./scripts/set-gsettings.sh  | tee -a /home/nyancat/systembuilder_output.log
 
 echo "$0 finished"
