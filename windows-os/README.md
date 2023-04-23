beba2d907c8950de8d6bec983669e892df911655
DrewHans <programmer.drew.hans@gmail.com>
2023-04-23 15:14:23 -0500

    Fix typos in enable-natural-scrolling.ps1

---
+++ windows-os/README.md
@@ -0,0 +1,10 @@
+# windows-os systembuilder
+
+This systembuilder script just installs software packages & programming language packages with [chocolatey](https://chocolatey.org/).
+
+### Enabling natural scrolling for the mouse's scrollwheel
+
+If you want to change the scroll direction on your mouse, open a powershell terminal as administrator and run the following command:
+`powershell -executionpolicy bypass -File .\enable-natural-scrolling.ps1`
+
+Then reboot the system and your scroll wheel direction should be reversed.
--- windows-os/enable-natural-scrolling.ps1
+++ windows-os/enable-natural-scrolling.ps1
@@ -1,4 +1,4 @@
-Write-Host "Starting $MyInvocation.MyCommand.Name";
+Write-Host "Starting $PSCommandPath";
 
 # GetCurrent() returns a WindowsIdentity object that represents the current Windows user
 $CurrentWindowsIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent();
@@ -8,23 +8,21 @@ $CurrentWindowsPrincipal = New-Object System.Security.Principal.WindowsPrincipal
 
 # check user is running this script with admin privileges
 if ($CurrentWindowsPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "This script is running with administrative privileges" -ForegroundColor Green;
+    Write-Host "This script is running with administrative privileges";
 } else {
-    Write-Warning "Insufficient permissions to run this script." -ForegroundColor Red;
-    Write-Warning "Open the PowerShell console as an administrator and try running this script again." -ForegroundColor Red;
+    Write-Warning "Insufficient permissions to run this script.";
+    Write-Warning "Open the PowerShell console as an administrator and try running this script again.";
     Exit;
 }
 
 Write-Host "Enabling natural scrolling for the mouse scrollwheel (will take effect after reboot).";
 
 Get-PnpDevice -Class Mouse -PresentOnly -Status OK | ForEach-Object {
-    "$($_.Name): $($_.DeviceID)";
-
     # set FlipFlopWheel DWORD to 1 (0 is default scroll direction, 1 is reversed direction [aka natural scrolling])
     Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Enum\$($_.DeviceID)\Device Parameters" -Name FlipFlopWheel -Value 1;
 }
 
-Write-Host "Finished $MyInvocation.MyCommand.Name";
+Write-Host "Finished $PSCommandPath";
 
 # source 1: https://dotnet-helpers.com/powershell/how-to-check-if-a-powershell-script-is-running-with-admin-privileges/
 # source 2: https://answers.microsoft.com/en-us/windows/forum/all/reverse-mouse-wheel-scroll/657c4537-f346-4b8b-99f8-9e1f52cd94c2
