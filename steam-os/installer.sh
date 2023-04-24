950af366ac9f5430cd44bdb2a92f8638349897da
DrewHans <programmer.drew.hans@gmail.com>
2023-04-23 22:30:00 -0500

    Update steam-os installer.sh

--- steam-os/installer.sh
+++ steam-os/installer.sh
@@ -28,16 +28,31 @@ echo "Starting $0"
 check_dependency "flatpak"
 check_not_root
 
+# software packages
 install_flatpak "com.github.tchx84.Flatseal"
+install_flatpak "org.mozilla.firefox"
 install_flatpak "io.gitlab.librewolf-community"
 install_flatpak "org.freedesktop.Platform.ffmpeg-full"
 install_flatpak "org.gnome.baobab"  # Disk Usage Analyzer
 install_flatpak "org.keepassxc.KeePassXC"
-install_flatpak "org.libretro.RetroArch"
-install_flatpak "org.ppsspp.PPSSPP"  # Sony PSP Emulator
-install_flatpak "net.rpcs3.RPCS3"  # Sony PS3 Emulator
-install_flatpak "org.yuzu_emu.yuzu"  # Nintendo Switch Emulator
+install_flatpak "com.belmoussaoui.Authenticator"  # Gnome Authenticator
+
+# games
 install_flatpak "com.mojang.Minecraft"
 install_flatpak "net.minetest.Minetest"
+# install_flatpak "net.veloren.airshipper"  # official Veloren launcher
+# install_flatpak "org.xonotic.Xonotic"  # FPS game
+
+# emulators
+install_flatpak "org.libretro.RetroArch"
+install_flatpak "com.snes9x.Snes9x"  # SNES Emulator
+install_flatpak "org.ppsspp.PPSSPP"  # Sony PSP Emulator
+install_flatpak "net.pcsx2.PCSX2"  # Sony PS2 Emulator
+install_flatpak "net.rpcs3.RPCS3"  # Sony PS3 Emulator
+install_flatpak "io.github.simple64.simple64"  # Nintendo 64 Emulator
+install_flatpak "io.mgba.mGBA"  # Nintendo Game Boy Advance Emulator
+install_flatpak "org.DolphinEmu.dolphin-emu"  # Nintendo GameCube / Wii Emulator
+install_flatpak "org.citra_emu.citra"  # Nintendo 3DS Emulator
+install_flatpak "org.yuzu_emu.yuzu"  # Nintendo Switch Emulator
 
 echo "$0 finished"
