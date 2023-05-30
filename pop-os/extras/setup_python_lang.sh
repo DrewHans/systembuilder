f7f1194175e0cff8d0bbd16b3e7ad92c1a676bcd
DrewHans <programmer.drew.hans@gmail.com>
2023-05-30 14:05:05 -0500

    Update setup_python_lang.sh

--- pop-os/extras/setup_python_lang.sh
+++ pop-os/extras/setup_python_lang.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
 
-# Optional step: enable deadsnakes ppa
-# get access to newer Python releases not available in default package manager
-# sudo add-apt-repository ppa:deadsnakes/ppa --yes --no-update
-
 sudo apt update
 
 # install newest python3 and pip3 using apt
 sudo apt install python3 python3-pip
 
-command -v python >/dev/null 2>&1 && {
-	echo "python version installed:"
-	python --version
-	echo ""
-}
+echo "python version installed:"
+python --version
+echo ""
 
-command -v pip3 >/dev/null 2>&1 && {
-	echo "pip3 version installed:"
-	pip3 --version
-	echo ""
-}
+echo "pip3 version installed:"
+pip3 --version
+echo ""
 
 # upgrade pip to newest pip version
 pip3 install --upgrade pip
 
-# install venv so we can setup virtual python environments
-sudo pip3 install virtualenv
+# install pipx so we can install python apps in isolated environments
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+
+# upgrade pipx to newest version
+#python3 -m pip install --user --upgrade pipx
 
 echo "pip packages installed:"
 pip list
+echo ""
+
+echo "pipx packages installed:"
+pipx list
 echo ""
 
 echo "Finished"
