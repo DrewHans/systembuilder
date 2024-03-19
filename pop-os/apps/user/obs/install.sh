9b82fae2ff8936d95df05be3453d750151840022
DrewHans <programmer.drew.hans@gmail.com>
2024-03-19 12:55:21 -0500

    Update obs install method

--- pop-os/apps/user/obs/install.sh
+++ pop-os/apps/user/obs/install.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 
-# don't install flatpak version, it has major problems with device detection
-# flatpak install --user flathub com.obsproject.Studio --assumeyes
+flatpak install --user flathub com.obsproject.Studio --assumeyes
