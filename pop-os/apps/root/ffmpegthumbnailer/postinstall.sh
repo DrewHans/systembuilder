30972c95cec4316dc305e94c682bc36a70e8f19c
DrewHans <programmer.drew.hans@gmail.com>
2024-03-30 17:38:35 -0500

    Make ffmpegthumbnailer default for totem

--- pop-os/apps/root/ffmpegthumbnailer/postinstall.sh
+++ pop-os/apps/root/ffmpegthumbnailer/postinstall.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+
+
+# replace totem-video-thumbnailer call with ffmpegthumbnailer
+patternMatch="^Exec=\/usr\/bin\/totem-video-thumbnailer -s %s %u %o$"
+replacementText="Exec=ffmpegthumbnailer -s %s -i %i -o %o -c png -f"
+sedPattern="/$patternMatch/c\\$replacementText"
+
+sed -i "$sedPattern" /usr/share/thumbnailers/totem.thumbnailer
